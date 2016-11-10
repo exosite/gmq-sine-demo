5b600feba1ab813937d820d16862514608fde54d
Will Charlton <willcharlton@exosite.com>
2016-11-10 12:28:27 -0600

    Updated to bypass gmq and no longer using hard-coded product.ids.

--- gmq_demo.py
+++ gmq_demo.py
@@ -2,23 +2,55 @@
 
 # pylint: disable=W0312
 
-import requests, time
+import requests, time, sys
+from urllib import urlencode
 from math import *
 
-def main():
+def main(product_id, serial, USING_GMQ):
+
+    try:
+        cik = open('gmq_demo.cik', 'r').read()
+    except IOError:
+        cik = ''
+
+
+    if 40 != len(cik):
+        stuff = urlencode({'vendor':product_id, 'model':product_id, 'sn':serial})
+        r = requests.post(
+            'https://m2.exosite.com/provision/activate',
+            headers={
+                'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8', 
+                'Content-Length': len(stuff)
+            },
+            data=stuff
+        )
+
+        print(r.text)
+        with open('gmq_demo.cik','w') as cikfile:
+            cikfile.write(r.text)
+
+    # sys.exit(1)
 
     while True:
         data = int(100 * cos(radians(time.clock()) * 100))
         print("Writing {} to gmq.".format(data))
 
         r = requests.post(
-            "http://localhost:8090/onep:v1/stack/alias",
-            headers={'X-Exosite-VMS': 'dubhxzv0r4e1m7vi dubhxzv0r4e1m7vi 12345'},
+            "https://{}.m2.exosite.com/onep:v1/stack/alias".format(product_id),
+            headers= {'X-Exosite-VMS': '{} {} {}'.format(product_id, product_id, serial)} if USING_GMQ else {'X-Exosite-CIK': '{}'.format(cik)},
             data={'test':data}
         )
         print(r)
         time.sleep(5)
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) < 3:
+        print("usage: {} <PRODUCT_ID> <SERIAL>".format(sys.argv[0]))
+        sys.exit(-1)
+
+    USING_GMQ = False
+    if len(sys.argv) == 4:
+        USING_GMQ = True if sys.argv[3] == "USING_GMQ" else False
+
+    main(sys.argv[1], sys.argv[2], USING_GMQ)
 
